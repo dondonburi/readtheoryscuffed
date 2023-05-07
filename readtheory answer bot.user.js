927012df7390911f841c28fe55d3cc29a8dc0988
dondonburi <129163115+dondonburi@users.noreply.github.com>
2023-05-07 18:24:01 +0800

    Add files via upload

--- readtheory answer bot.user.js
+++ readtheory answer bot.user.js
@@ -19,7 +19,7 @@
     setTimeout(() => {
         //Grade 12 quizzes
         const Paints = {quiztitle: '"Paints"', quizanswers: ["E","B","C","A","E","C","A","C"]};
-        const ABeetleBox = {quiztitle: "A Beetle in a Box", quizanswers: ["C","D","B","B"]};
+        const ABeetleBox = {quiztitle: "A Beetle in a Box", quizanswers: ["C","D","E","B"]};
         const AGoodDeath = {quiztitle: "A Good Death", quizanswers: ["C","D","B","C","B","E","C","E"]};
         const ALackBias = {quiztitle: "A Lack of Bias", quizanswers: ["E","D","A","B","C"]};
         const APresTrad = {quiztitle: "A Presidential Tradition: Reaching the People", quizanswers: ["C","A","E","B","D","C","C","A"]};
